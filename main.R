8975a7b00f132fb4fc77f2362384fe3e1fc3fb15
Igor Barinov <bis@hintsolutions.ru>
2014-05-31 17:59:56 +0400

    next

--- main.R
+++ main.R
@@ -2,6 +2,9 @@
 #reading 100 from 100k.csv, with separator
 #you should save 100k.csv without BOM before importing
 records <- read.csv("data/100k.csv",sep ="|", header= TRUE, nrows=100)
-
 #reading diapazons of payments (min,max)
 diapazon <- read.csv("catalog/diapazon.csv",sep=",", header=TRUE)
+#reading categories of providers
+providers <- read.csv("catalog/categories.csv",sep="|",header=TRUE)
+
+
