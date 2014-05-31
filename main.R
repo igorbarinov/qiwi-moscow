449119c021b30eeaafbca036d0989a9148828e0e
Igor Barinov <bis@hintsolutions.ru>
2014-05-31 16:20:23 +0400

    added diapazon.csv

--- main.R
+++ main.R
@@ -1,4 +1,7 @@
+#data
 #reading 100 from 100k.csv, with separator
 #you should save 100k.csv without BOM before importing
-
 records <- read.csv("data/100k.csv",sep ="|", header= TRUE, nrows=100)
+
+#reading diapazons of payments (min,max)
+diapazon <- read.csv("catalog/diapazon.csv",sep=",", header=TRUE)
